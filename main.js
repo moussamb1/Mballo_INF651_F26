08b7c11995b1800d188e782bb9553701c5e166e2
Moussa Mballo <moussa@Nitro.local>
2026-09-15 15:51:08 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "pngimg.com - bmw_logo_PNG19713.png") {
+    myImage.setAttribute("src", "Bmw-Logo-PNG-Image.png");
+  } else {
+    myImage.setAttribute("src", "pngimg.com - bmw_logo_PNG19713.png");
+  }
+});
+
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.addEventListener("click", () => {
+  setUserName();
+});
